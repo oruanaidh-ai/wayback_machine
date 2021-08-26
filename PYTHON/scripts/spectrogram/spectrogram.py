2d2cbeff81b8b474855ed4c1c7d9fa71c284dfd6
Joseph O Ruanaidh <joseph.oruanaidh@gmail.com>
2021-08-26 16:45:15 -0500

    No longer require sys.path to be edited Reduced the number of frequencies in spectrogram to save memory multiprocessing is a little faster than no multiprocessing

--- PYTHON/scripts/spectrogram/spectrogram.py
+++ PYTHON/scripts/spectrogram/spectrogram.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 from multiprocessing import Pool, cpu_count
@@ -5,22 +6,24 @@ from functools import partial
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pathlib import Path
 
-sys.path.append('/Users/oruanaidh/projects/wayback_machine/PYTHON/utils')
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.join(*current_dir.split(os.path.sep)[:-2])
+sys.path.append( str( Path('/') / Path(parent_dir) / 'utils' ) )
 
 from tools.dsp.sliding_window import SlidingWindow
 
 
 def get_interval_times(t, stride, width):
-
     times = SlidingWindow(t, stride=stride, width=width)
-
     return np.mean([*times], axis=-1)
 
 
-def power_spectrum(data, window):
-    fft = np.fft.rfft(data * window)
-    return np.abs(fft)
+def power_spectrum(data, window, fs, maxF):
+    fft = np.fft.rfft((data - np.mean(data)) * window)
+    crop = int( maxF/fs * len(data) )
+    return np.abs(fft)[:crop]
 
 
 def timer(func, *args, **kwargs):
@@ -34,24 +37,23 @@ def timer(func, *args, **kwargs):
 
 
 @timer
-def spectrogram_multiprocess(data, fs, t):
+def spectrogram_multiprocess(data, fs, t, maxF):
     stride = fs
     width = fs*30
     slider = SlidingWindow(data, stride=stride, width=width)
 
     window = np.hamming(width)
-    kwargs = {'window': window}
-    my_power_spectrum = partial(power_spectrum, **kwargs)
-
-    pool = Pool(cpu_count())
 
     times = get_interval_times(t, stride, width)
 
-    return np.array( pool.map(my_power_spectrum, slider) ), times
+    with Pool(cpu_count()) as pool:
+        spec = pool.map(partial(power_spectrum, window=window, fs=fs, maxF=maxF),  slider, chunksize=5000)
+
+    return np.array( spec ), times
 
 
 @timer
-def spectrogram(data, fs, t):
+def spectrogram(data, fs, t, maxF):
 
     stride = fs
     width = fs*30
@@ -61,15 +63,16 @@ def spectrogram(data, fs, t):
 
     window = np.hamming(width)
 
-    return np.array([power_spectrum(s, window) for s in slider]), times
+    spec = [power_spectrum(s, window, fs, maxF) for s in slider]
+    return np.array(spec), times
 
 
-def draw_spectrogram(spec, fs, times, filename):
+def draw_spectrogram(spec, fs, times, maxF, filename):
 
     fig, ax = plt.subplots(nrows=1, ncols=1)
 
     _, N = spec.shape
-    freqs = np.arange(N)*fs/2/N
+    freqs = np.arange(N)/N*maxF
 
     times, freqs = np.meshgrid(times, freqs)
 
@@ -77,33 +80,36 @@ def draw_spectrogram(spec, fs, times, filename):
     ax.set_ylabel('Frequency')
     ax.set_xlabel('Time')
 
-
     plt.savefig(filename, dpi=150)
     #plt.show()
 
 
-if __name__ == '__main__':
-
-    fs = 128
-    N = fs*900
+def main():
+    fs = 150
+    N = fs*3600
+    maxF = 3
 
     t = np.linspace(0, N, N*fs)
-    f_init = 10
-    f_final = 50
+    f_init = 1
+    f_final = 2
     alpha = (f_final - f_init)/N
 
     data = np.sin(2*np.pi*(f_init*t + 0.5*alpha*t*t)) + np.random.normal(len(t))
 
     print('Multiprocessing')
-    spec, times = spectrogram_multiprocess(data, fs=fs, t=t)
+    spec, times = spectrogram_multiprocess(data, fs=fs, t=t, maxF=maxF)
 
-    draw_spectrogram( 10*np.arcsinh(spec), fs=fs, times=times, filename='spectrum_mp.png')
+    #draw_spectrogram( np.arcsinh(spec), fs=fs, times=times, filename='spectrum_mp.png')
 
 
     print('Single CPU')
-    spec, times = spectrogram(data, fs=fs, t=t)
+    spec, times = spectrogram(data, fs=fs, t=t, maxF=maxF)
 
-    draw_spectrogram( 10*np.arcsinh(spec), fs=fs, times=times, filename='spectrum.png')
+    draw_spectrogram( np.arcsinh(spec*100), fs=fs, times=times, maxF=maxF, filename='spectrum.png')
+    
+    
+if __name__ == '__main__':
 
+    main()
 
 
